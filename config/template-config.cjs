a40a3fe5a073ca181a220c0bd61076e5757f34c5
Mmiaozii <152631826+Mmiaozii@users.noreply.github.com>
2023-12-01 23:52:03 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -3,7 +3,43 @@ const TEMPLATE_CONFIG = [
   {
      id: '0001',
 
-    title: '洁子同学！晚上好呀！'
+    title: '洁子同学！晚上好呀！',
+    desc: `
+      
+      **现在是{{date.DATA}}**
+    
+  
+      {{holidaytts.DATA}}
+
+      要记着吃早餐！！！
+      --
+      --
+      
+      {{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      风向: {{wind_direction.DATA}}
+      
+      风级: {{wind_scale.DATA}}
+
+      -
+      
+      {{comprehensive_horoscope.DATA}}
+      
+      -
+      
+      {{birthday_message.DATA}}
+      
+      -
+      
+      {{poetry_title.DATA}} 
+      {{poetry_content.DATA}}
+      
+      {{moment_copyrighting.DATA}}
+    `
     
     
   },
